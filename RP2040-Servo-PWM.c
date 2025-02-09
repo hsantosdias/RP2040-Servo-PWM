098e7bf03e85f893a3e17196a0d9e566d93b55bf
Hugo S. Dias <39714844+hsantosdias@users.noreply.github.com>
2025-02-08 21:01:53 -0300

    initialize project structure with VSCode configuration and basic LED blinking example

---
+++ RP2040-Servo-PWM.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+
+
+int LED_PIN = 13;
+
+
+int main () {
+    stdio_init_all();
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (1) {
+        gpio_put(LED_PIN, 1);
+        sleep_ms(1000);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(1000);
+    }
+    return 0;
+}
+
+
+
